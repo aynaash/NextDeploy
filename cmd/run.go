a46b17c0c38424b2d5f7d01e1480773ffda7d28e
aynaash <caynaashow@gmail.com>
2025-06-12 11:35:59 +0300

    add runimage command

---
+++ cmd/run.go
@@ -0,0 +1,107 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"nextdeploy/internal/git"
+	"nextdeploy/internal/logger"
+	"nextdeploy/internal/nextdeploy"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+type Config struct {
+	Image string `yaml:"image"`
+}
+
+var (
+	runLogger  = logger.PackageLogger("RunImage", "🚀 Run Image")
+	configFile string
+)
+
+var runimageCmd = &cobra.Command{
+	Use:   "runimage",
+	Short: "Run Docker image with configuration from YAML",
+	Long: `Reads image name from YAML config, gets tag from git commit or flag,
+and runs the Docker container with environment variables from Doppler.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		runImage()
+	},
+}
+
+func init() {
+	runimageCmd.Flags().StringVarP(&configFile, "config", "c", "config.yaml", "Path to YAML config file")
+	runimageCmd.Flags().StringVarP(&tag, "tag", "t", "", "Custom tag for the Docker image")
+	rootCmd.AddCommand(runimageCmd)
+}
+
+func runImage() {
+	// Read config file
+	config, err := nextdeploy.Load("nextdeploy.yml")
+	if err != nil {
+		fmt.Printf("Error reading config: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Get tag (from flag or git)
+	imageTag := tag
+	if imageTag == "" {
+		imageTag, err = git.GetCommitHash()
+		if err != nil {
+			fmt.Printf("Error getting git commit hash: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	// Download Doppler secrets
+	err = downloadDopplerSecrets()
+	if err != nil {
+		fmt.Printf("Error downloading Doppler secrets: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Run Docker container
+	fullImage := fmt.Sprintf("%s:%s", config.Docker.Image, imageTag)
+	err = runDockerContainer(fullImage)
+	if err != nil {
+		fmt.Printf("Error running Docker container: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Docker container started successfully")
+}
+
+func downloadDopplerSecrets() error {
+	cmd := exec.Command("doppler", "secrets", "download", "--no-file", "--format", "env")
+	output, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("doppler command failed: %w", err)
+	}
+
+	err = os.WriteFile(".env", output, 0644)
+	if err != nil {
+		return fmt.Errorf("could not write .env file: %w", err)
+	}
+
+	return nil
+}
+
+func runDockerContainer(image string) error {
+	absPath, err := filepath.Abs(".env")
+	if err != nil {
+		return fmt.Errorf("could not get absolute path for .env: %w", err)
+	}
+
+	cmd := exec.Command("docker", "run", "-p", "3000:3000", "--env-file", absPath, image)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("docker run failed: %w", err)
+	}
+	runLogger.Success("Docker container started with image: %s", image)
+
+	return nil
+}
